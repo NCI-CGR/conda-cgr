dc65ce2b92163d8ff9704efb53b096c78ee695ef
Kevin Liao <liaoks@ccad2.cm.cluster>
2024-01-11 15:40:45 -0500

    modify grafpop build to copy SaveSamples.pl

--- recipes/grafpop1.0/build.sh
+++ recipes/grafpop1.0/build.sh
@@ -1,6 +1,8 @@
 mkdir $PREFIX/bin
 cp grafpop $PREFIX/bin
 
+sed 's/\/usr\/local\/bin\/perl/\/usr\/bin\/env perl/' SaveSamples.pl > $PREFIX/bin/SaveSamples.pl
+
 mkdir $PREFIX/share
 mkdir $PREFIX/share/grafpop
 cp -R data/* $PREFIX/share/grafpop
